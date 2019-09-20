38975dc0a5b6c98fa2a8400a7afe3e8c4e6724f9
熊甜 <wb.xiongtian01@mesg.corp.netease.com>
2019-09-20 11:29:04 +0800

    01 初始化

--- src/App.js
+++ src/App.js
@@ -1,26 +1,11 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React,{useState} from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+    const {count, setCount} = useState(0)
+    return (
+        <div>
+            我是函数式组件
+            <div>你点击了</div>
+        </div>
+    )
 }
-
-export default App;
